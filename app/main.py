80d0572bc542973cfaab3be6048d52faef827be8
Michael Lynch <git@mtlynch.io>
2021-02-03 16:41:02 -0500

    Create edid maker

--- app/main.py
+++ app/main.py
@@ -2,8 +2,7 @@
 
 import argparse
 import logging
-
-import dummy
+import sys
 
 logger = logging.getLogger(__name__)
 
@@ -19,14 +18,30 @@ def configure_logging():
     root_logger.setLevel(logging.INFO)
 
 
+def parse_edid():
+    return sys.stdin.readline().strip().lower().replace(' ', '')
+
+
+def print_cmd(edid):
+    print('EDID=$(mktemp)')
+    max_chars = 60
+    for i in range(int(len(edid) / max_chars) + 1):
+        start = i * max_chars
+        end = start + max_chars
+        snippet = edid[start:end]
+        print('echo "%s" >> $EDID' % snippet)
+    print('sudo v4l2-ctl --device=/dev/video0 --set-edid=file="$EDID" '
+          '--fix-edid-checksums')
+
+
 def main(args):
     configure_logging()
     logger.info('Started runnning')
-    print(dummy.dummy())
+    print_cmd(parse_edid())
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        prog='Python Seed',
+        prog='Make EDID',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     main(parser.parse_args())
