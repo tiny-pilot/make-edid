b9f7421ec76758ff2af821509256f517ef022bf6
Michael Lynch <git@mtlynch.io>
2021-02-18 13:40:43 -0500

    Update functionality

--- README.md
+++ README.md
@@ -1,18 +1,8 @@
 # Make EDID
 
-## Customization
-
-To customize this for your project:
-
-1. Change `COPYRIGHT` to your name.
-1. Change `LICENSE` to [a license of your choosing](https://choosealicense.com/).
-1. Change the CircleCI badge in `README.md` to your own Circle CI project badge.
-1. Change the app name in `app/main.py` from `Python Seed` to your app's name.
-1. Rename `app/dummy.py` and `tests/test_dummy.py` to the module names of your choosing.
-1. Begin working.
-
 ## Run
 
 ```bash
-./app/main.py
+. venv/bin/activate
+echo "someedid" | ./app/main.py
 ```
--- app/main.py
+++ app/main.py
@@ -19,18 +19,19 @@ def configure_logging():
 
 
 def parse_edid():
-    return sys.stdin.readline().strip().lower().replace(' ', '')
+    return sys.stdin.read().strip().lower().replace(' ', '').replace('\n', '')
 
 
 def print_cmd(edid):
-    print('EDID=$(mktemp)')
-    max_chars = 60
+    print('readonly EDID="/home/ustreamer/edids/tc358743-edid.hex"')
+    print('echo -ne "" | sudo tee "${EDID}" && \\')
+    max_chars = 40
     for i in range(int(len(edid) / max_chars) + 1):
         start = i * max_chars
         end = start + max_chars
         snippet = edid[start:end]
-        print('echo "%s" >> $EDID' % snippet)
-    print('sudo v4l2-ctl --device=/dev/video0 --set-edid=file="$EDID" '
+        print('  echo \'%s\' | sudo tee -a "${EDID}" && \\' % snippet)
+    print('  sudo v4l2-ctl --device=/dev/video0 --set-edid=file="${EDID}" '
           '--fix-edid-checksums')
 
 
